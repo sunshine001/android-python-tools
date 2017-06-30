09626c94ecb4d7c586fad2c9b80c878869c6957b
cisco_luo <895275020@qq.com>
2017-06-30 11:53:08 +0800

    Improved code.

---
+++ dimens-transform.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+import codecs
+import  xml.dom.minidom
+
+DPR = 1.5
+
+#open xml document
+dom = xml.dom.minidom.parse('dimens.xml')
+root = dom.documentElement
+nodes = root.getElementsByTagName("dimen")
+
+for node in nodes:
+	nodename = node.getAttribute('name')
+	nodevalue = node.childNodes[0].nodeValue
+	if nodevalue.endswith('dp'):
+		num = nodevalue.replace('dp', '')
+		node.childNodes[0].nodeValue = int(round(float(num)/DPR));
+
+newfile = codecs.open('new_dimens.xml', 'w', 'utf-8')
+dom.writexml(newfile, encoding='utf-8')
